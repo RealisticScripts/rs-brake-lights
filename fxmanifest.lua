566f9ea056b436a2573e1dc664b6fe70c5d7a0b3
RealisticScripts <realisticscripts@gmail.com>
2026-04-12 09:35:24 -0700

    Sync version check and manifest to v1.0.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 'yes'
 author 'Realistic Scripts'
 name 'RS Brake Lights'
 description 'A realistic vehicle brake light script.'
-version '1.0.0'
+version 'v1.0.0'
 repository 'https://github.com/RealisticScripts/rs-brake-lights'
 license 'MIT'
 
